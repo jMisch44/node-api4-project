ead4de7824067adf867779844b362bf42fba0a32
jMisch44 <jennimisch@gmail.com>
2021-09-30 15:16:43 -0400

    two post endpoints created

--- index.js
+++ index.js
@@ -8,13 +8,30 @@ server.use(cors());
 server.use(express.json());
 
 const userData = [
-  { id: 0, userName: "Bob" },
-  { id: 1, userName: "John" },
-  { id: 2, userName: "Jane" },
+  { id: 0, userName: "Bob", password: "fake0" },
+  { id: 1, userName: "John", password: "fake1" },
+  { id: 2, userName: "Jane", password: "fake2" },
 ];
+
+function addUser(user) {
+  return userData.push(user);
+}
+
+function userLogin(user) {
+  return { message: `Hello ${user.userName}` };
+}
 
 server.get("/api/users", (req, res) => {
   res.status(200).json(userData);
+});
+
+server.post("/api/register", (req, res) => {
+  addUser(req.body);
+  res.status(201).json(req.body);
+});
+
+server.post("/api/login", (req, res) => {
+  res.status(201).json(userLogin(req.body));
 });
 
 server.get("*", (req, res) => {
