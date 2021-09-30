211d065285ff0e1bbe099d6a9993881c29780844
jMisch44 <jennimisch@gmail.com>
2021-09-30 17:30:53 -0400

    added bcrypt for posted passwords

--- index.js
+++ index.js
@@ -1,37 +1,68 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const bcrypt = require("bcrypt");
 
 const server = express();
+
+const hashPassword = async (password, saltRounds = 10) => {
+  try {
+    const salt = await bcrypt.genSalt(saltRounds);
+    return await bcrypt.hash(password, salt);
+  } catch (err) {
+    console.log(err);
+  }
+  return null;
+};
+
+const comparePassword = async (password, hash) => {
+  try {
+    return await bcrypt.compare(password, hash);
+  } catch (err) {
+    console.log(err);
+  }
+  return false;
+};
 
 server.use(cors());
 server.use(express.json());
 
 const userData = [
-  { id: 0, userName: "Bob", password: "fake0" },
-  { id: 1, userName: "John", password: "fake1" },
-  { id: 2, userName: "Jane", password: "fake2" },
+  { id: 0, userName: "Bob", password: "" },
+  { id: 1, userName: "John", password: "" },
+  { id: 2, userName: "Jane", password: "" },
 ];
 
-function addUser(user) {
-  return userData.push(user);
+async function findUser(user) {
+  return await userData.find((users) => user.userName === users.userName);
 }
 
-function userLogin(user) {
-  return { message: `Hello ${user.userName}` };
+async function addUser(user) {
+  const hashedPass = await hashPassword(user.password);
+  userData.push({ ...user, password: hashedPass });
+  return userData[userData.length - 1];
+}
+
+async function userLogin(user) {
+  const dbUser = await findUser(user);
+  const validUser = await comparePassword(user.password, dbUser.password);
+  if (validUser) {
+    return { message: `Hello ${dbUser.userName}` };
+  } else {
+    return { message: `invalid password!!` };
+  }
 }
 
 server.get("/api/users", (req, res) => {
   res.status(200).json(userData);
 });
 
-server.post("/api/register", (req, res) => {
-  addUser(req.body);
-  res.status(201).json(req.body);
+server.post("/api/register", async (req, res) => {
+  res.status(201).json(await addUser(req.body));
 });
 
-server.post("/api/login", (req, res) => {
-  res.status(201).json(userLogin(req.body));
+server.post("/api/login", async (req, res) => {
+  res.status(201).json(await userLogin(req.body));
 });
 
 server.get("/", (req, res) => {
